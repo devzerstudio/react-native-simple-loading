c4caaff800d16db3e47937fb70e7f5e4e21a598f
hashaaan <shalithavidanapathirana@gmail.com>
2021-06-30 04:48:47 +0530

    Add stable loading component with prop validations

--- index.js
+++ index.js
@@ -1,27 +1,104 @@
 import React from "react";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Modal,
+  StyleSheet,
+  Text,
+  useWindowDimensions,
+  View,
+} from "react-native";
+import PropTypes from "prop-types";
 
 import colors from "./config/colors";
 
-export default function SimpleLoading({
+const ANIMATION = ["slide", "fade", "none"];
+const DIRECTIONS = ["column", "row"];
+const MODES = ["default", "fullscreen"];
+const SIZES = ["large", "small"];
+
+const Loading = ({
   containerStyles,
-  size,
+  textStyles,
+  color = "primary",
+  direction = DIRECTIONS[0],
+  height,
+  size = SIZES[0],
   text,
-  color,
   ...props
-}) {
+}) => {
   return (
-    <View style={[styles.container, containerStyles]}>
-      <ActivityIndicator size={size} color={colors[color]} {...props} />
-      {text && <Text>{text}</Text>}
+    <View
+      style={[
+        styles.container,
+        {
+          height,
+          flexDirection: direction,
+        },
+        containerStyles,
+      ]}>
+      <ActivityIndicator
+        size={size}
+        color={colors[color] || color}
+        {...props}
+      />
+      {text && <Text style={[styles.text, textStyles]}>{text}</Text>}
     </View>
   );
+};
+
+function SimpleLoading({
+  animation = ANIMATION[0],
+  loading = false,
+  mode = MODES[0],
+  ...props
+}) {
+  const { height } = useWindowDimensions();
+
+  if (loading) {
+    if (mode === "fullscreen") {
+      return (
+        <Modal
+          animationType="slide"
+          onRequestClose={() => {}}
+          supportedOrientations={["landscape", "portrait"]}
+          transparent={false}
+          visible={loading}
+          statusBarTranslucent={true}>
+          <Loading height={height} {...props} />
+        </Modal>
+      );
+    } else {
+      return <Loading {...props} />;
+    }
+  } else {
+    return null;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "center"
-  }
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.white,
+  },
+  text: {
+    padding: 10,
+    textAlign: "center",
+    color: colors.black,
+  },
 });
+
+SimpleLoading.propTypes = {
+  animation: PropTypes.oneOf(ANIMATION),
+  color: PropTypes.string,
+  containerStyles: PropTypes.object,
+  direction: PropTypes.oneOf(DIRECTIONS),
+  loading: PropTypes.bool.isRequired,
+  mode: PropTypes.oneOf(MODES),
+  size: PropTypes.oneOf(SIZES),
+  text: PropTypes.string,
+  textStyles: PropTypes.object,
+};
+
+export default SimpleLoading;
